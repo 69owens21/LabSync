f5bfdceb98737717bc9b1e0c1625f14c124980a5
69owens21 <gracie.owens@ace.tamut.edu>
2026-04-28 14:34:00 -0500

    Add stabilized Dusk auth tests and equipment migration

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    public function testBasicExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->assertSee('Email');
+        });
+    }
+}
